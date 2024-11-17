7b3a6fabe08ad9c624c3929ade84ec098bd3dd8d
jmgummer <joseaq08@gmail.com>
2024-11-17 16:50:57 +0300

    first commit

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import pyautogui
+import os
 
 screen_width, screen_height = pyautogui.size()
 resolution = (screen_width, screen_height)
@@ -9,11 +10,16 @@ name = input("Kindly Name Your Output File: ")
 fps = int(input("Set frames per second(FPS) recommended 30: "))
 recording_duration = int(input("Set recording duration(seconds): "))
 output_filename = name + ".mp4"
+capture_folder = "Capture"
 
+if not os.path.exists(capture_folder):
+    os.makedirs(capture_folder)
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 out = cv2.VideoWriter(output_filename, fourcc, fps, resolution)
 
 print("Starting To Capture "+ output_filename)
+
+output_filename = os.path.join(capture_folder, name + ".mp4")
 for _ in range(int(recording_duration * fps)):
     screen = pyautogui.screenshot()
     frame = np.array(screen)
@@ -21,3 +27,4 @@ for _ in range(int(recording_duration * fps)):
     out.write(frame)
 
 out.release()
+print(f"Recording saved in {output_filename}")
